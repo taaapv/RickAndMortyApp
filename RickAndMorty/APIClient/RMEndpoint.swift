c6d57ea50c36cbf303101a8747c769cbed3c19ed
TanyaKost <taaapv@gmail.com>
2023-02-28 23:34:18 +0400

    Add docstrings and API related files

---
+++ RickAndMorty/APIClient/RMEndpoint.swift
@@ -0,0 +1,20 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorty
+//
+//  Created by Татьяна on 28.02.2023.
+//
+
+import Foundation
+
+/// Represents unique API endpoints
+@frozen enum RMEndpoint: String {
+	/// Endpoint to get character info
+	case character
+	
+	/// Endpoint to get location info
+	case location
+	
+	/// Endpoint to get episode info
+	case episode
+}
--- RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 
+/// Controller to show and search for Episods
 final class RMEpisodeViewController: UIViewController {
 	
 	override func viewDidLoad() {
