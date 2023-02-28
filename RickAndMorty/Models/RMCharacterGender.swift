b52ac5b390b49196f32fdcdbfc3c41676ad6d030
TanyaKost <taaapv@gmail.com>
2023-03-01 01:14:16 +0400

    Add models

---
+++ RickAndMorty/Models/RMCharacterGender.swift
@@ -0,0 +1,15 @@
+//
+//  RMCharacterGender.swift
+//  RickAndMorty
+//
+//  Created by Татьяна on 28.02.2023.
+//
+
+import Foundation
+
+enum RMCharacterGender: String, Codable {
+	case male = "Male"
+	case female = "Female"
+	case genderless = "Genderless"
+	case `unknown` = "unknown"
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,13 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Татьяна on 28.02.2023.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+	let name: String
+	let url: String
+}
